8db511f0a91f6443232c3a8e135496def8930bf1
krthbk <kimsungkyurpg@gmail.com>
2026-02-04 09:43:27 +0100

    refactor: move home route to controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\HolidayController;
-use App\Models\TeamMember;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $members = TeamMember::orderBy('name')->get();
-    return view('welcome', compact('members'));
-});
-
+Route::get('/', [AttendanceController::class, 'index'])
+    ->name('home');
 
 // vytvoření záznamu
 Route::post('/attendance', [AttendanceController::class, 'store'])
@@ -20,10 +16,11 @@ Route::patch('/attendance/{attendance}', [AttendanceController::class, 'update']
 // smazání záznamu
 Route::delete('/attendance/{attendance}', [AttendanceController::class, 'destroy'])
     ->name('attendance.destroy');
-    
 
 // ✅ kalendář bude číst sem (všichni členové)
-Route::get('/attendance/events', [AttendanceController::class, 'events'])->name('attendance.events');
+Route::get('/attendance/events', [AttendanceController::class, 'events'])
+    ->name('attendance.events');
 
-Route::get('/holidays/cz', [HolidayController::class, 'cz'])->name('holidays.cz');
+Route::get('/holidays/cz', [HolidayController::class, 'cz'])
+    ->name('holidays.cz');
 
